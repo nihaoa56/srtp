fa1ad22c8d65d37daa50b547507045280f4e5241
nihaoa56 <1239588540@qq.com>
2019-12-13 21:50:57 +0800

    OK

--- cluster/GMM.py
+++ cluster/GMM.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
 from sklearn.metrics import homogeneity_completeness_v_measure
 from sklearn import mixture
@@ -12,30 +13,14 @@ from scipy.cluster.hierarchy import fcluster
 from sklearn import mixture
 from sklearn.cluster import MeanShift
 from sklearn.cluster import AgglomerativeClustering
-
-
-
-def hierarchy_classify(hierarchy_array, num_of_hierarchy):
-    '''
-    将聚好类的料面进行分组
-    '''
-    hierarchy = list()
-    hierarchy.append(list())
-    hierarchy.append(list())
-    hierarchy.append(list())
-    for i in range(num_of_hierarchy):
-        for j in range(len(hierarchy_array)):
-            if hierarchy_array[j] == i:
-                hierarchy[i].append(j)
-    return hierarchy
-
-
+from sklearn.decomposition import PCA   #pca降维工具
 
 
 
 path = 'data_in/data4.txt'
 source_data = np.loadtxt(path)
-data = source_data[..., 20:]
+data = source_data[..., 21:]
+print(data.shape)
 # 高斯混合聚类
 gmm=mixture.GaussianMixture(n_components=3)
 gmm.fit(data)
@@ -56,6 +41,63 @@ pred_ward = ward.fit_predict(data)
 print("层次聚类结果 :")
 print('层次:', np.unique(pred_ward))
 print(pred_ward)
+
+
+# pca降维
+pca_machine = PCA(n_components=3,whiten=True)
+pca_machine.fit(data)
+
+pca_result = pca_machine.fit_transform(data)
+# print(pca_result)
+# print(pca_result[:,0])
+# pca_X = pca_result[:, 0]
+# pca_Y = pca_result[:, 1]
+# pca_Z = pca_result[:,2]
+
+class_one = np.arange(3) #矩阵无效初始化
+class_two = np.arange(3)
+class_three = np.arange(3)
+
+
+
+for i in range(len(pred_gmm)):
+    if pred_gmm[i] == 0:
+        class_one = np.vstack((class_one,pca_result[i]))
+    elif pred_gmm[i] == 1:
+        class_two = np.vstack((class_two,pca_result[i]))
+    elif pred_gmm[i] == 2:
+        class_three = np.vstack((class_three, pca_result[i]))
+        
+# 矩阵处理
+class_one = np.delete(class_one, 0, 0)
+class_two = np.delete(class_two, 0, 0)
+class_three = np.delete(class_three, 0, 0)
+
+class_one_X = class_one[:, 0]
+class_one_Y = class_one[:, 1]
+class_one_Z = class_one[:, 2]
+
+class_two_X = class_two[:, 0]
+class_two_Y = class_two[:, 1]
+class_two_Z = class_two[:, 2]
+
+class_three_X = class_three[:, 0]
+class_three_Y = class_three[:, 1]
+class_three_Z = class_three[:, 2]
+
+
+
+ax = plt.subplot(111, projection='3d') # 创建一个三维的绘图工程
+# 将数据点分成三部分画，在颜色上有区分度
+ax.scatter(class_one_X, class_one_Y, class_one_Z, c='y') # 绘制数据点
+ax.scatter(class_two_X, class_two_Y, class_two_Z, c='r')
+ax.scatter(class_three_X, class_three_Y, class_three_Z, c='g')
+
+ax.set_zlabel('Z') # 坐标轴
+ax.set_ylabel('Y')
+ax.set_xlabel('X')
+plt.savefig('d.jpg')
+plt.show()
 
 # classed_hierarchy_gmm = hierarchy_classify(pred_gmm, 3)
 # # classed_hierarchy_gmm_array = np.array(classed_hierarchy_gmm)
